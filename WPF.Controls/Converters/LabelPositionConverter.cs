ac1a62989b92708469d9c63c710353c001e66996
yemikudaisi <yemikudaisi@gmail.com>
2016-12-21 12:02:13 +0100

    Initial Commit

---
+++ WPF.Controls/Converters/LabelPositionConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WPF.Controls.Enums;
+
+namespace WPF.Controls.Converters
+{
+    public class LabelPositionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            LabelPosition orientation = (LabelPosition)value;
+
+            if (orientation == LabelPosition.Left)
+                return Orientation.Horizontal;
+            else
+                return Orientation.Vertical;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF.Controls/LabeledTextBox.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF.Controls.Enums;
+
+namespace WPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for LabeledTextBox.xaml
+    /// </summary>
+    public partial class LabeledTextBox : UserControl
+    {
+        public LabeledTextBox()
+        {
+            InitializeComponent();
+        }
+        #region Label DependencyProperty
+
+        /// <summary>
+        /// Gets or sets the Label for the text box
+        /// </summary>
+        public String Label
+        {
+            get { return (String)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string),
+              typeof(LabeledTextBox), new PropertyMetadata(""));
+
+        #endregion
+
+        #region Value Dependency Propery
+
+        /// <summary>
+        /// Gets or sets the text value of the text box
+        /// </summary>
+        public object Value
+        {
+            get { return (object)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(object),
+              typeof(LabeledTextBox), new PropertyMetadata(null));
+
+        #endregion
+
+        #region LabelPosition Dependency Propery
+
+        /// <summary>
+        /// Gets or sets the postion the label will be located relative to the text box
+        /// </summary>
+        public LabelPosition LabelPosition
+        {
+            get { return (LabelPosition)GetValue(LabelPositionProperty); }
+            set { SetValue(LabelPositionProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelPositionProperty =
+            DependencyProperty.Register("LabelPosition", typeof(object),
+              typeof(LabeledTextBox), new PropertyMetadata(LabelPosition.Left));
+
+        #endregion
+    }
+}
